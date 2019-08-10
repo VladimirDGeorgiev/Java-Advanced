3cc0dbf6038226a7a6e9c7c67b1cf2011ff4f904
VladimirDGeorgiev <vladogeorgiev222@abv.bg>
2019-08-10 13:46:27 +0300

    exr

--- JavaOOp/ExamPrepJavaOPP/MortalEngines/core/MachinesManagerImpl.java
+++ JavaOOp/ExamPrepJavaOPP/MortalEngines/core/MachinesManagerImpl.java
@@ -13,6 +13,8 @@ import entities.interfaces.Tank;
 
 import java.util.Map;
 
+import static common.OutputMessages.*;
+
 public class MachinesManagerImpl implements MachinesManager {
 
     private PilotFactory pilotFactory;
@@ -37,12 +39,12 @@ public class MachinesManagerImpl implements MachinesManager {
     public String hirePilot(String name) {
 
         if (this.pilots.containsKey(name)){
-            return String.format(OutputMessages.pilotExists,name);
+            return String.format(pilotExists,name);
 
         }else{
             Pilot pilot = this.pilotFactory.createPilot(name);
             this.pilots.put(pilot.getName(),pilot);
-            return String.format(OutputMessages.pilotHired,name);
+            return String.format(pilotHired,name);
         }
     }
 
@@ -50,11 +52,11 @@ public class MachinesManagerImpl implements MachinesManager {
     public String manufactureTank(String name, double attackPoints, double defensePoints) {
 
         if (this.machines.containsKey(name)){
-            return String.format(OutputMessages.machineExists,name);
+            return String.format(machineExists,name);
         }else{
             Tank tank = this.machineFactory.createTank(name, attackPoints, defensePoints);
             this.machines.put(name,tank);
-            return String.format(OutputMessages.tankManufactured,name,attackPoints,defensePoints);
+            return String.format(tankManufactured,name,attackPoints,defensePoints);
         }
 
     }
@@ -62,39 +64,39 @@ public class MachinesManagerImpl implements MachinesManager {
     @Override
     public String manufactureFighter(String name, double attackPoints, double defensePoints) {
         if (this.machines.containsKey(name)) {
-            return String.format(OutputMessages.machineExists,name);
+            return String.format(machineExists,name);
         }else{
             Fighter fighter = this.machineFactory.createFighter(name,attackPoints,defensePoints);
             this.machines.put(name,fighter);
-            return String.format(OutputMessages.fighterManufactured,name,attackPoints,defensePoints);
+            return String.format(fighterManufactured,name,attackPoints,defensePoints);
         }
     }
 
     @Override
     public String engageMachine(String selectedPilotName, String selectedMachineName) {
         if (!this.machines.containsKey(selectedMachineName)){
-            return String.format(OutputMessages.machineNotFound,selectedMachineName);
+            return String.format(machineNotFound,selectedMachineName);
         }
         if (!this.pilots.containsKey(selectedPilotName)){
-            return String.format(OutputMessages.pilotNotFound,selectedPilotName);
+            return String.format(pilotNotFound,selectedPilotName);
         }
         if (this.machines.get(selectedMachineName).getPilot()!=null){
-            return String.format(OutputMessages.machineHasPilotAlready,selectedMachineName);
+            return String.format(machineHasPilotAlready,selectedMachineName);
         }
         Machine machine = this.machines.get(selectedMachineName);
         Pilot pilot = this.pilots.get(selectedPilotName);
         this.pilots.get(selectedPilotName).addMachine(machine);
         this.machines.get(selectedMachineName).setPilot(pilot);
-        return String.format(OutputMessages.machineEngaged,selectedPilotName,selectedMachineName);
+        return String.format(machineEngaged,selectedPilotName,selectedMachineName);
     }
 
     @Override
     public String attackMachines(String attackingMachineName, String defendingMachineName) {
         if (!this.machines.containsKey(attackingMachineName)){
-            return String.format(OutputMessages.machineNotFound,attackingMachineName);
+            return String.format(machineNotFound,attackingMachineName);
         }
         if (!this.machines.containsKey(defendingMachineName)){
-            return String.format(OutputMessages.machineNotFound,defendingMachineName);
+            return String.format(machineNotFound,defendingMachineName);
         }
         this.machines.get(attackingMachineName).attack(defendingMachineName);
 
@@ -109,7 +111,7 @@ public class MachinesManagerImpl implements MachinesManager {
                 this.machines.get(defendingMachineName).setHealthPoints(0);
             }
         }
-        return String.format(OutputMessages.attackSuccessful,
+        return String.format(attackSuccessful,
                 defendingMachineName,
                 attackingMachineName,
                 this.machines.get(defendingMachineName).getHealthPoints());
@@ -119,7 +121,7 @@ public class MachinesManagerImpl implements MachinesManager {
     @Override
     public String pilotReport(String pilotName) {
         if (!this.pilots.containsKey(pilotName)) {
-            return String.format(OutputMessages.pilotNotFound,pilotName);
+            return String.format(pilotNotFound,pilotName);
         }
         return this.pilots.get(pilotName).report();
     }
@@ -127,26 +129,26 @@ public class MachinesManagerImpl implements MachinesManager {
     @Override
     public String toggleFighterAggressiveMode(String fighterName) {
         if (!this.machines.containsKey(fighterName)){
-            return String.format(OutputMessages.machineNotFound,fighterName);
+            return String.format(machineNotFound,fighterName);
         }
         if (this.machines.get(fighterName).getClass().getSimpleName().equals("TankImpl")){
-            return String.format(OutputMessages.notSupportedOperation,fighterName);
+            return String.format(notSupportedOperation,fighterName);
         }else{
             ((Fighter) this.machines.get(fighterName)).toggleAggressiveMode();
-            return String.format(OutputMessages.fighterOperationSuccessful,fighterName);
+            return String.format(fighterOperationSuccessful,fighterName);
         }
     }
 
     @Override
     public String toggleTankDefenseMode(String tankName) {
         if (!this.machines.containsKey(tankName)){
-            return String.format(OutputMessages.machineNotFound,tankName);
+            return String.format(machineNotFound,tankName);
         }
         if (!this.machines.get(tankName).getClass().getSimpleName().equals("TankImpl")){
-            return String.format(OutputMessages.notSupportedOperation,tankName);
+            return String.format(notSupportedOperation,tankName);
         }else{
             ((Tank) this.machines.get(tankName)).toggleDefenseMode();
-            return String.format(OutputMessages.tankOperationSuccessful,tankName);
+            return String.format(tankOperationSuccessful,tankName);
         }
     }
 }
